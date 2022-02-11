43a926e2a7c2fc75be46f3ea9758cc620bc32fcc
ScarletDragon <scarletdragon278@gmail.com>
2022-02-11 19:47:23 -0300

    Actualizacion

--- core/src/personajes/Atalante.java
+++ core/src/personajes/Atalante.java
@@ -1,5 +1,8 @@
 package personajes;
  
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
+
 import utiles.Imagen;
 
 public class Atalante extends personajePrefab{
@@ -7,6 +10,48 @@ public class Atalante extends personajePrefab{
 
    
 
+    }
+
+    @Override
+    public Object getValue(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void putValue(String key, Object value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setEnabled(boolean b) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 
 
